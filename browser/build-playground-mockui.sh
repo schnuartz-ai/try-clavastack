226f428109cf31ca9311f12a1f81a31317f0b4ab
Schnuartz AI <schnuartz.ai@gmail.com>
2026-09-17 12:46:11 +0200

    Build themes for the standard Schnuartz variant

--- browser/build-playground-mockui.sh
+++ browser/build-playground-mockui.sh
@@ -106,7 +106,7 @@ test "$(emcc --version | head -1 | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -1)"
 make -C "$FORK_SRC" build-i18n
 # Both MockUI forks load their compiled default theme from /flash/themes during
 # startup. Omitting this step makes SpecterGui fail before MOCKUI_READY.
-if [[ "$1" = k9ert ]]; then
+if [[ "$1" != schnuartz-alternative ]]; then
   make -C "$FORK_SRC" build-themes
 fi
 python3 "$ROOT/browser/prepare-mockui.py" "$FORK_SRC"
